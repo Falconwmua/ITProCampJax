3901ee18548a06a7326993888f44acf2fce4c758
Will Murphy <wmurphy@acuitybrands.com>
2016-11-18 09:36:27 -0500

    ATLTS

--- ARMTemplate/demo.ps1
+++ ARMTemplate/demo.ps1
@@ -3,18 +3,18 @@ Import-Module AzureRM.Profile
 Import-Module AzureRM.Resources
 
 
-$Azurecred = Get-Credential
+$Azurecred = Get-Credential wwm01@acuitysso.com
 $subscriptionid = '76d3e108-7f58-4f95-8dab-206757f82929'
 
 Login-AzureRmAccount -Credential $Azurecred
 
 Get-AzureRmSubscription -SubscriptionId $subscriptionid | Select-AzureRmSubscription
 
-$resourcegroupname = 'ITProCampJax'
+$resourcegroupname = 'ATLTS2016'
 $location = 'westus2'
-$sitename = 'itprocampjaxd4a01'
-$hostingplan = 'ITPC4A01'
-$sqlServerName = 'itprocampjax'
+$sitename = 'atlts2016ab'
+$hostingplan = 'atlts2016abhp'
+$sqlServerName = 'atlts2016absql'
 $sqlAdministratorLogin = 'AdminWill'
 $sqlAdministratorPassword = ConvertTo-SecureString -String "P@ssw0rd" -AsPlainText -Force
 $repoUrl = "https://github.com/ProjectNami/projectnami"
